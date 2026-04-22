f7c06fbae463702dfc296a91e5d76be1334ce9d7
Bolat Zhora <zhorabolat@gmail.com>
2026-04-22 09:51:44 +0500

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QBitQuiz from "./QBitQuiz.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QBitQuiz />
+);
